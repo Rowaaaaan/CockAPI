d46d05e1daf11695fc62ff24f7c407a6fe881386
Rowan <82100519+Rowaaaaan@users.noreply.github.com>
2024-02-18 09:33:16 +0800

    feat: add cock mongoose model

---
+++ src/models/cockModel.ts
@@ -0,0 +1,59 @@
+import { Schema, model } from "mongoose";
+import ICock from "../interfaces/cockInterface";
+
+const cockSchema: Schema = new Schema<ICock>(
+	{
+		imageUrl: Array<String>,
+		breed: {
+			type: String,
+			required: true
+		},
+		names: {
+			type: [String],
+			required: true
+		},
+		scientificName: {
+			type: String,
+			required: true
+		},
+		classification: {
+			APA: String,
+			EE: String,
+			PCGB: String
+		},
+		yearDiscovered: Number,
+		countryOfOrigin: {
+			type: String,
+			required: true
+		},
+		conservationStatus: String,
+		averageHeight: {
+			male: Number,
+			female: Number
+		},
+		averageWeight: {
+			male: Number,
+			female: Number
+		},
+		skinColor: Array<String>,
+		eggColor: Array<String>,
+		combType: String,
+		description: {
+			type: String,
+			required: true
+		},
+		meta: {
+			type: {
+				sources: Array<String>
+			},
+			default: {
+				sources: [""]
+			}
+		},
+	},
+	{
+		timestamps: true
+	}
+);
+
+export default model<ICock>("Cock", cockSchema);
